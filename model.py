7c7caca3ef265f43f59f97d3466e2d75028730d5
Raisin228 <bogdanatrosenko@gmail.com>
2026-06-08 14:46:21 +0300

    Feature: реализовал сеть из 4х слоёв [1 входной (2 нейрона), 2 скрытых (64 нейрона), 1 выходной (4 нейрона)].

---
+++ model.py
@@ -0,0 +1,47 @@
+"""Модель MLP. Атрошенко Б. С."""
+
+import torch
+import torch.nn as nn
+
+
+class Network(nn.Module):
+    """
+    Нейросеть для ф-ии Q(s, a).
+
+    Вход: состояние агента.
+    Выход:
+    """
+
+    def __init__(self, state_size=2, hidden_size=64, action_size=4):
+        """
+        Инициализатор.
+
+        :param state_size: кол-во входных параметров.
+        :param hidden_size: кол-во нейронов в скрытом слое.
+        :param action_size: кол-во выходных параметров.
+        """
+
+        super(Network, self).__init__()
+        self.network = nn.Sequential(
+            nn.Linear(state_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, action_size),
+        )
+
+    def forward(self, state: torch.Tensor):
+        """
+        Прямой проход: принимает состояние, возвращает Q-значения.
+
+        :param state: тензор с координатами агента.
+        """
+
+        return self.network(state)
+
+
+model = Network()
+st = torch.tensor([0.8, 0.0], dtype=torch.float32)
+q_values = model(st)
+
+print(q_values)
